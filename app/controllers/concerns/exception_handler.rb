7198469ea2f1b568bf84961d5d350b20b437647d
Michael_Scheer <macas.scheer@gmail.com>
2020-07-08 13:00:51 -0700

    include Response and Exceptionhandler modules in applicationController

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,7 +1,9 @@
 module ExceptionHandler
     # provides the more graceful 'included' method
     extend ActiveSupport::Concern
-
+    # create method of Todos Controller we use create! which will raise an exception'
+    # This way we can avoid deep nested if statements in the controller
+    # so we rescue from this exception
     included do
         rescue_from ActiveRecord::RecordNotFound do |e|
             json_response({message: e.message}, :not_found)
